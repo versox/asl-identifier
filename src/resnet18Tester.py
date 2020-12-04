3b378777859bb675a476cddcfec427943128ecde
SWrinn <35043165+SWrinn@users.noreply.github.com>
2020-12-04 00:05:51 -0500

    Fixed training data path and added resenet18 model tester
    ** Note there will be an error after running the tester code because there are not at least 1000 pictures **

--- src/resnet18Tester.py
+++ src/resnet18Tester.py
@@ -8,12 +8,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.ImageFolder(root='./asl_alphabet_train/asl_alphabet_train/', transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.ImageFolder(root='./asl_alphabet_test/asl_alphabet_test/', transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
@@ -28,51 +29,7 @@ def imshow(img):
 
 if __name__ == "__main__":
     torch.multiprocessing.freeze_support()
-    
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
 
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-    # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-    
-    #net = Net()
-    net = models.resnet18(pretrained=True)
-    # KEEP THIS IN MIND - if using different pretrained models, the name for the final layer may be different
-    net.fc = nn.Linear(512, 29)
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    for epoch in range(2):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 1 == 0:    # print every mini-batch
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-    print('Finished Training')
-    
-    PATH = './resnet18ASL_alphabet.pth'
-    torch.save(net.state_dict(), PATH)
-    """
     dataiter = iter(testloader)
     images, labels = dataiter.next()
 
@@ -80,7 +37,10 @@ if __name__ == "__main__":
     imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-    net = Net()
+    PATH = './resnet18ASL_alphabet.pth'
+
+    net = models.resnet18(pretrained=True)
+    net.fc = nn.Linear(512, 29)
     net.load_state_dict(torch.load(PATH))
 
     outputs = net(images)
@@ -120,4 +80,3 @@ if __name__ == "__main__":
     for i in range(10):
         print('Accuracy of %5s : %2d %%' % (
             classes[i], 100 * class_correct[i] / class_total[i]))
-    """
