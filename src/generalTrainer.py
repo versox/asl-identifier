cd5e864f5e476783635e387e40c976a81649ca19
SWrinn <35043165+SWrinn@users.noreply.github.com>
2020-12-08 20:15:12 -0500

    added code to train using GPU, increased batch size and  increased number of decimal places shown

--- src/generalTrainer.py
+++ src/generalTrainer.py
@@ -13,10 +13,10 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.ImageFolder(root='./asl_alphabet_train/asl_alphabet_train/', transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.ImageFolder(root='./asl_alphabet_test/asl_alphabet_test/', transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space')
 
@@ -28,6 +28,9 @@ def imshow(img):
 
 if __name__ == "__main__":
     torch.multiprocessing.freeze_support()
+
+    #train on GPU instead of CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     dataiter = iter(trainloader)
     images, labels = dataiter.next()
@@ -49,10 +52,12 @@ if __name__ == "__main__":
         net.fc = nn.Linear(512, 29)
     """
     
-    #net = Net()
     net = models.resnet18(pretrained=True)
     # KEEP THIS IN MIND - if using different pretrained models, the name for the final layer may be different
     net.fc = nn.Linear(512, 29)
+
+    #train on GPU instead of CPU
+    net.to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
@@ -63,6 +68,10 @@ if __name__ == "__main__":
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+
+            #GPU, not CPU
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -75,8 +84,8 @@ if __name__ == "__main__":
 
             # print statistics
             running_loss += loss.item()
-            if i % 1 == 0:    # print every mini-batch
-                print('[%d, %5d] loss: %.3f' %
+            if i % 100 == 0:    # print every mini-batch
+                print('[%d, %5d] loss: %.15f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
 
@@ -86,52 +95,3 @@ if __name__ == "__main__":
     PATH = './resnet18ASL_alphabet.pth'
 
     torch.save(net.state_dict(), PATH)
-    """
-    dataiter = iter(testloader)
-    images, labels = dataiter.next()
-
-    # print images
-    imshow(torchvision.utils.make_grid(images))
-    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-    net = Net()
-    net.load_state_dict(torch.load(PATH))
-
-    outputs = net(images)
-
-    _, predicted = torch.max(outputs, 1)
-
-    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                                for j in range(4)))
-
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
-
-    class_correct = list(0. for i in range(10))
-    class_total = list(0. for i in range(10))
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            outputs = net(images)
-            _, predicted = torch.max(outputs, 1)
-            c = (predicted == labels).squeeze()
-            for i in range(4):
-                label = labels[i]
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
-
-
-    for i in range(10):
-        print('Accuracy of %5s : %2d %%' % (
-            classes[i], 100 * class_correct[i] / class_total[i]))
-    """
